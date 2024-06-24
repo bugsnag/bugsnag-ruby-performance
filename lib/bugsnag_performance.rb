713e84283cd4e08c10113acf03035591ed916045
Joe Haines <hello@joehaines.co.uk>
2024-06-24 12:26:47 +0100

    Move BugsnagPerformance::Error to its own file

--- lib/bugsnag_performance.rb
+++ lib/bugsnag_performance.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
+require_relative "bugsnag_performance/error"
 require_relative "bugsnag_performance/version"
 require_relative "bugsnag_performance/configuration"
-
-module BugsnagPerformance
-  class Error < StandardError; end
-end
