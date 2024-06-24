658f19be4615735e5e50936797af7a6ac54032f3
Joe Haines <hello@joehaines.co.uk>
2024-06-24 12:30:03 +0100

    Add BugsnagPerformance::ConfigurationValidator
    Usage:
    
      result = BugsnagPerformance::ConfigurationValidator.validate(configuration)
    
      # '#valid?' and '#messages' can be used to report validation failures
      # to the user
      raise "Invalid configuration: #{result.messages}" unless result.valid?
    
      # get the validated configuration object, i.e. with default values
      # instead of invalid ones
      validated_configuration = result.configuration
    
      # some validation failures mean we shouldn't send any data to bugsnag,
      # which can be checked using '#send_traces?'
      should_send_traces = result.send_traces?

--- lib/bugsnag_performance.rb
+++ lib/bugsnag_performance.rb
@@ -3,3 +3,4 @@
 require_relative "bugsnag_performance/error"
 require_relative "bugsnag_performance/version"
 require_relative "bugsnag_performance/configuration"
+require_relative "bugsnag_performance/configuration_validator"
