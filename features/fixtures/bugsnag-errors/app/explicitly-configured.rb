2997e686ff32d75d2a4e3c7bab3120ae13777f05
Joe Haines <hello@joehaines.co.uk>
2024-06-25 12:18:32 +0100

    Add MazeRunner test for bugsnag errors integration

---
+++ features/fixtures/bugsnag-errors/app/explicitly-configured.rb
@@ -0,0 +1,25 @@
+require "uri"
+require "json"
+require "bugsnag"
+require "bugsnag_performance"
+
+Bugsnag.configure do |configuration|
+  configuration.api_key = "123456789012345678901234567890ab"
+  configuration.app_version = "1.2.3"
+  configuration.release_stage = "prodevelopment"
+  configuration.enabled_release_stages = ["prodevelopment", "production"]
+end
+
+configuration = BugsnagPerformance::Configuration.new(Bugsnag.configuration)
+
+uri = URI(ENV.fetch('MAZE_RUNNER_ENDPOINT'))
+uri.path = '/reflect'
+
+body = JSON.generate({
+  api_key: configuration.api_key,
+  app_version: configuration.app_version,
+  release_stage: configuration.release_stage,
+  enabled_release_stages: configuration.enabled_release_stages,
+})
+
+Net::HTTP.post(uri, body, { 'content-type' => 'application/json' })
