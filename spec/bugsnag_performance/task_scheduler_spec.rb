841870f3a497c13147a7f968022b09936040b28c
Joe Haines <hello@joehaines.co.uk>
2024-07-12 10:03:08 +0100

    Add TaskScheduler
    This exists primarily to make it easier to test classes that consume
    Tasks - they can accept a TaskScheduler instance which can then be
    mocked to avoid having to deal with asynchronous Task execution

---
+++ spec/bugsnag_performance/task_scheduler_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+RSpec.describe BugsnagPerformance::TaskScheduler do
+  context "#now" do
+    it "returns a task scheduled to run immediately" do
+      callback = spy(Proc)
+
+      task = subject.now { callback.call }
+
+      expect(task).to be_a(BugsnagPerformance::Task)
+
+      # wait for the scheduled task to fire
+      elapsed = measure { task.wait }
+
+      expect(callback).to have_received(:call).once
+      expect(elapsed).to be_within(0.1).of(0)
+    end
+  end
+end
