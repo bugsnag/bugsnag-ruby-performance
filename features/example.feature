cc421b5f45fc2c412ce3d771b28ec06caaf8d24f
Joe Haines <hello@joehaines.co.uk>
2024-07-25 12:05:24 +0100

    Add Maze Runner test using the SpanExporter

--- features/example.feature
+++
@@ -1,5 +0,0 @@
-Feature: example
-
-Scenario: It runs the basic app
-  Given I run the service "basic" with the command "bundle exec ruby app.rb"
-  And I wait to receive a reflection
--- features/fixtures/basic/app/app.rb
+++ features/fixtures/basic/app/app.rb
@@ -1,7 +1,41 @@
 require "uri"
 require "open-uri"
+require "bugsnag_performance"
+require "opentelemetry/sdk"
 
-uri = URI(ENV.fetch('MAZE_RUNNER_ENDPOINT'))
-uri.path = '/reflect'
+configuration = BugsnagPerformance::Configuration.new(BugsnagPerformance::NilErrorsConfiguration.new)
+configuration.api_key = "ffffffffffffffffffffffffffffffff"
+configuration.endpoint = "#{ENV.fetch('MAZE_RUNNER_ENDPOINT')}/traces"
 
-uri.read
+delivery = BugsnagPerformance::Delivery.new(configuration)
+scheduler = BugsnagPerformance::TaskScheduler.new
+fetcher = BugsnagPerformance::ProbabilityFetcher.new(delivery, scheduler)
+manager = BugsnagPerformance::ProbabilityManager.new(fetcher)
+payload_encoder = BugsnagPerformance::PayloadEncoder.new
+header_encoder = BugsnagPerformance::SamplingHeaderEncoder.new
+
+batch_processor = OpenTelemetry::SDK::Trace::Export::BatchSpanProcessor.new(
+  BugsnagPerformance::SpanExporter.new(manager, delivery, payload_encoder, header_encoder)
+)
+
+OpenTelemetry::SDK.configure do |otel_configurator|
+  otel_configurator.service_name = "basic app"
+
+  otel_configurator.add_span_processor(batch_processor)
+  otel_configurator.resource = OpenTelemetry::SDK::Resources::Resource.create({
+    # these resource attributes are required for Maze Runner validation
+    "device.id" => 1,
+    "deployment.environment" => "production",
+  })
+end
+
+OpenTelemetry.tracer_provider.sampler = BugsnagPerformance::Sampler.new(manager)
+Tracer = OpenTelemetry.tracer_provider.tracer("maze tracer")
+
+5.times do |i|
+  Tracer.in_span("test span #{i + 1}") do |span|
+    span.set_attribute("span.custom.age", i * 10)
+  end
+end
+
+batch_processor.shutdown
