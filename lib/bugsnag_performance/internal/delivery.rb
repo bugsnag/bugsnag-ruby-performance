c53cf464df673bbb505b20feda38cd1e3f326df3
Daria Bialobrzeska <daria.bialobrzeska@smartbear.com>
2024-11-08 18:18:43 +0100

    Disable maze trace validation until PIPE-7498 is done

--- lib/bugsnag_performance/internal/delivery.rb
+++ lib/bugsnag_performance/internal/delivery.rb
@@ -17,7 +17,7 @@ module BugsnagPerformance
       def deliver(headers, body)
         headers = headers.merge(
           @common_headers,
-        # TODO - add this back when pipeline is ready
+        # TODO - can be restored after https://smartbear.atlassian.net/browse/PIPE-7498
         #  { "Bugsnag-Sent-At" => Time.now.utc.iso8601(3) },
         )
 
